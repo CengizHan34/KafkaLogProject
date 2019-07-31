df007117439adc99b8d0d31a1e26e8c6c4744732
Cengiz HAN <cengiz.han@hotmail.co.uk>
2019-07-31 20:24:31 +0300

    master

---
+++ src/main/java/com/kafka/log/KafkaLogProject/KafkaLogProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.log.KafkaLogProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaLogProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaLogProjectApplication.class, args);
+	}
+
+}
